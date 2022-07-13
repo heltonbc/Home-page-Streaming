cc3287ce9b0990d61fb1582431ecc5eefc3cebcc
Helton Cunha <helton.bc01@gmail.com>
2022-07-13 19:09:53 -0400

    implementacao do react-slick, html feito, faltando parte do css, fixed #2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import NavComponents from "./components/NavComponents";
 import HeroSlider from "./components/HeroSlider";
-import Footer from "./components/Footer";
+/* import Footer from "./components/Footer";
+import Home from "./components/Footer"; */
 
 function App() {
     return (
         <>
             <NavComponents />
             <HeroSlider />
-            <Footer />
+            {/* 
+            <Home />
+            <Footer /> */}
         </>
     );
 }
