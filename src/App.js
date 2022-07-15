08a20a83a4191aaa4a32ea8578694f90567c72f7
Helton Cunha <helton.bc01@gmail.com>
2022-07-14 22:57:31 -0400

    dados iniciais para começar o trabalho com o slick-carousel, incluindo o footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import NavComponents from "./components/NavComponents";
 import HeroSlider from "./components/HeroSlider";
-/*import Footer from "./components/Footer";*/
+import Footer from "./components/Footer";
 
 function App() {
     return (
         <>
             <NavComponents />
             <HeroSlider />
-            {/* <Footer /> */}
+            <Footer />
         </>
     );
 }
