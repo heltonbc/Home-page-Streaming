53624a2e6927bdd50d82903514ecda8eb7a277b0
Helton Cunha <helton.bc01@gmail.com>
2022-07-17 22:43:23 -0400

    adicionado novas categorias de filmes

---
+++ src/films_Category/Classic.js
@@ -0,0 +1,60 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import "../App.css";
+import datafilms from "../components/data";
+
+function Classic() {
+    const settings = {
+        dots: false,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        initialSlide: 0,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    };
+
+    return (
+        <div className="Classic">
+            <h5 className="title-category">Filmes de Comédia</h5>
+            <Slider {...settings}>
+                {datafilms.map(item => (
+                    <div className="card">
+                        <div className="card-top">
+                            <img src={item.img} alt={item.exemple} />
+                        </div>
+                        <div className="card-bottom"></div>
+                    </div>
+                ))}
+            </Slider>
+        </div>
+    );
+}
+
+export default Classic;
